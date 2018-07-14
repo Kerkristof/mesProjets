10ea1b414bc78ee165c7df71d54f3102f37f2b64
kerkristof <kerkristof@gmail.com>
2018-07-14 10:18:26 +0200

    ajout d'unfichier monTchat

---
+++ monTchat/public/client.js
@@ -0,0 +1,9 @@
+$(function(){
+
+    // var socket = io.connect('http://localhost:3000');
+    $('#btn').on('click', function () {
+        alert('le btn fonctionne');
+        // socket.emit('message','salut serveur');
+    });
+    
+});
--- monTchat/server.js
+++ monTchat/server.js
@@ -1,29 +1,37 @@
-var http = require('http');
-var fs = require('fs');
-
-// Chargement du fichier index.html affiché au client
-var server = http.createServer(function (req, res) {
-    fs.readFile('./index.html', 'utf-8', function (error, content) {
-        res.writeHead(200, {
-            "Content-Type": "text/html"
-        });
-        res.end(content);
-    });
-});
-
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
 // Chargement de socket.io
-var io = require('socket.io').listen(server);
+var io = require('socket.io')(server);
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+    });
+
 
 // Quand un client se connecte, on le note dans la console
-io.sockets.on('connection', function (socket) {
+io.on('connection', function (socket) {
     console.log('Un client est connecté !');
 });
 
-io.sockets.on('connection', function (chaussette) {
-    chaussette.on('message', function (msg) {
-        console.log('message: ' + msg);
-    });
+server.listen(3000, function(){
+    console.log('listening on : 3000');
 });
 
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
 
-server.listen(3000);
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+// });
+
+// http.listen(3000, function(){
+//   console.log('listening on *:3000');
+// });
